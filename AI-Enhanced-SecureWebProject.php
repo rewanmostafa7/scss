38f71766560453ff4b2d25accae711f454b7fb64
rewanmostafa7 <rn0108899@icloud.com>
2026-02-26 14:57:47 +0200

    Add files via upload

---
+++ AI-Enhanced-SecureWebProject.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AI-Enhanced-SecureWebProject</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;900&display=swap" rel="stylesheet">        rel="stylesheet">
+    <link rel="stylesheet" href="Assets/style-first-page.css">
+</head>
+
+<body>
+    <div class="page-wrapper">
+        <header>
+            <div class="container">
+                <nav>
+                    <h1>AI-Enhanced-SecureWebProject</h1>
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <section class="hero"> <!-- Landing page -->
+            <div class="container">
+                <h2><span>AI-Powered</span>Secure Web Application for <br>Cybersecurity Training</h2>
+                <p>An educational cybersecurity platform that compares vulnerable and secure implementations of web
+                    , enhanced with AI-based threat detection and attack monitoring mechanisms.</p>
+                <button class="btn-red">Launch Vulnerable Environment</button>
+                <button class="btn-blue">Launch Secure Environment</button>
+            </div>
+        </section>  
+    </div>
+</body>
+<!--//linear-gradient(0deg, rgba(#000,0), rgba(#000,.5)), -->
+</html>
